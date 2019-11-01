891ec1dcf8da82fbe187842b456c601486be6f8f
rounakSingh-Tuts <rounak.techinfo@gmail.com>
2019-11-01 10:39:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-kbgvte
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-kbgvte)
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//For multiple html tags we have to wrap it in div
+ReactDOM.render(<div><h1>Hello World</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
